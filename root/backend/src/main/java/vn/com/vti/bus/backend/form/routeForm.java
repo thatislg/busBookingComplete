ee9a9f714f819d5777edc5b9f0b2b8d052172d59
Vu165 <85608418+Vu165@users.noreply.github.com>
2022-10-29 17:13:22 +0900

    add routeForm

---
+++ root/backend/src/main/java/vn/com/vti/bus/backend/form/routeForm.java
@@ -0,0 +1,42 @@
+package vn.com.vti.bus.backend.form;
+
+import java.security.Timestamp;
+import java.sql.Date;
+
+public class routeForm {
+	private Integer price;
+	private Date operationStartDate;
+	private Date operationEndDate;
+	private Timestamp scheduledDeapartureTime;
+	private Timestamp scheduledArrivalTime;
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Date getOperationStartDate() {
+		return operationStartDate;
+	}
+	public void setOperationStartDate(Date operationStartDate) {
+		this.operationStartDate = operationStartDate;
+	}
+	public Date getOperationEndDate() {
+		return operationEndDate;
+	}
+	public void setOperationEndDate(Date operationEndDate) {
+		this.operationEndDate = operationEndDate;
+	}
+	public Timestamp getScheduledDeapartureTime() {
+		return scheduledDeapartureTime;
+	}
+	public void setScheduledDeapartureTime(Timestamp scheduledDeapartureTime) {
+		this.scheduledDeapartureTime = scheduledDeapartureTime;
+	}
+	public Timestamp getScheduledArrivalTime() {
+		return scheduledArrivalTime;
+	}
+	public void setScheduledArrivalTime(Timestamp scheduledArrivalTime) {
+		this.scheduledArrivalTime = scheduledArrivalTime;
+	}
+}
