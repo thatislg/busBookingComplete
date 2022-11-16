4aad6783ba01728fcd631aae92049bd3c5f63910
thatislg <lnllnl01111@gmail.com>
2022-11-16 21:05:15 +0900

    Cập nhật route data

--- root/common/sql/3_member_data.sql
+++ root/common/sql/3_member_data.sql
@@ -1,1 +1,199 @@
 -- MEMBER TABLE'S SAMPLES DATA
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id001','123456','Việt Nam Name 001','member001@gmail.com','070-3548-0001');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id002','123456','Việt Nam Name 002','member002@gmail.com','070-3548-0002');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id003','123456','Việt Nam Name 003','member003@gmail.com','070-3548-0003');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id004','123456','Việt Nam Name 004','member004@gmail.com','070-3548-0004');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id005','123456','Việt Nam Name 005','member005@gmail.com','070-3548-0005');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id006','123456','Việt Nam Name 006','member006@gmail.com','070-3548-0006');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id007','123456','Việt Nam Name 007','member007@gmail.com','070-3548-0007');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id008','123456','Việt Nam Name 008','member008@gmail.com','070-3548-0008');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id009','123456','Việt Nam Name 009','member009@gmail.com','070-3548-0009');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id010','123456','Việt Nam Name 010','member010@gmail.com','070-3548-0010');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id011','123456','Việt Nam Name 011','member011@gmail.com','070-3548-0011');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id012','123456','Việt Nam Name 012','member012@gmail.com','070-3548-0012');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id013','123456','Việt Nam Name 013','member013@gmail.com','070-3548-0013');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id014','123456','Việt Nam Name 014','member014@gmail.com','070-3548-0014');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id015','123456','Việt Nam Name 015','member015@gmail.com','070-3548-0015');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id016','123456','Việt Nam Name 016','member016@gmail.com','070-3548-0016');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id017','123456','Việt Nam Name 017','member017@gmail.com','070-3548-0017');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id018','123456','Việt Nam Name 018','member018@gmail.com','070-3548-0018');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id019','123456','Việt Nam Name 019','member019@gmail.com','070-3548-0019');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id020','123456','Việt Nam Name 020','member020@gmail.com','070-3548-0020');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id021','123456','日本 Name 021','member021@gmail.com','070-3548-0021');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id022','123456','日本 Name 022','member022@gmail.com','070-3548-0022');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id023','123456','日本 Name 023','member023@gmail.com','070-3548-0023');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id024','123456','日本 Name 024','member024@gmail.com','070-3548-0024');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id025','123456','日本 Name 025','member025@gmail.com','070-3548-0025');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id026','123456','日本 Name 026','member026@gmail.com','070-3548-0026');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id027','123456','日本 Name 027','member027@gmail.com','070-3548-0027');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id028','123456','日本 Name 028','member028@gmail.com','070-3548-0028');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id029','123456','日本 Name 029','member029@gmail.com','070-3548-0029');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id030','123456','日本 Name 030','member030@gmail.com','070-3548-0030');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id031','123456','日本 Name 031','member031@gmail.com','070-3548-0031');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id032','123456','日本 Name 032','member032@gmail.com','070-3548-0032');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id033','123456','日本 Name 033','member033@gmail.com','070-3548-0033');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id034','123456','日本 Name 034','member034@gmail.com','070-3548-0034');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id035','123456','日本 Name 035','member035@gmail.com','070-3548-0035');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id036','123456','日本 Name 036','member036@gmail.com','070-3548-0036');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id037','123456','日本 Name 037','member037@gmail.com','070-3548-0037');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id038','123456','日本 Name 038','member038@gmail.com','070-3548-0038');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id039','123456','日本 Name 039','member039@gmail.com','070-3548-0039');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id040','123456','日本 Name 040','member040@gmail.com','070-3548-0040');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id041','123456','日本 Name 041','member041@gmail.com','070-3548-0041');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id042','123456','日本 Name 042','member042@gmail.com','070-3548-0042');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id043','123456','日本 Name 043','member043@gmail.com','070-3548-0043');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id044','123456','日本 Name 044','member044@gmail.com','070-3548-0044');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id045','123456','日本 Name 045','member045@gmail.com','070-3548-0045');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id046','123456','日本 Name 046','member046@gmail.com','070-3548-0046');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id047','123456','日本 Name 047','member047@gmail.com','070-3548-0047');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id048','123456','日本 Name 048','member048@gmail.com','070-3548-0048');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id049','123456','日本 Name 049','member049@gmail.com','070-3548-0049');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id050','123456','日本 Name 050','member050@gmail.com','070-3548-0050');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id051','123456','English Name 051','member051@gmail.com','070-3548-0051');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id052','123456','English Name 052','member052@gmail.com','070-3548-0052');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id053','123456','English Name 053','member053@gmail.com','070-3548-0053');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id054','123456','English Name 054','member054@gmail.com','070-3548-0054');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id055','123456','English Name 055','member055@gmail.com','070-3548-0055');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id056','123456','English Name 056','member056@gmail.com','070-3548-0056');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id057','123456','English Name 057','member057@gmail.com','070-3548-0057');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id058','123456','English Name 058','member058@gmail.com','070-3548-0058');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id059','123456','English Name 059','member059@gmail.com','070-3548-0059');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id060','123456','English Name 060','member060@gmail.com','070-3548-0060');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id061','123456','English Name 061','member061@gmail.com','070-3548-0061');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id062','123456','English Name 062','member062@gmail.com','070-3548-0062');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id063','123456','English Name 063','member063@gmail.com','070-3548-0063');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id064','123456','English Name 064','member064@gmail.com','070-3548-0064');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id065','123456','English Name 065','member065@gmail.com','070-3548-0065');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id066','123456','English Name 066','member066@gmail.com','070-3548-0066');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id067','123456','English Name 067','member067@gmail.com','070-3548-0067');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id068','123456','English Name 068','member068@gmail.com','070-3548-0068');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id069','123456','English Name 069','member069@gmail.com','070-3548-0069');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id070','123456','English Name 070','member070@gmail.com','070-3548-0070');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id071','123456','English Name 071','member071@gmail.com','070-3548-0071');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id072','123456','English Name 072','member072@gmail.com','070-3548-0072');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id073','123456','English Name 073','member073@gmail.com','070-3548-0073');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id074','123456','English Name 074','member074@gmail.com','070-3548-0074');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id075','123456','English Name 075','member075@gmail.com','070-3548-0075');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id076','123456','English Name 076','member076@gmail.com','070-3548-0076');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id077','123456','English Name 077','member077@gmail.com','070-3548-0077');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id078','123456','English Name 078','member078@gmail.com','070-3548-0078');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id079','123456','English Name 079','member079@gmail.com','070-3548-0079');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id080','123456','English Name 080','member080@gmail.com','070-3548-0080');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id081','123456','Member Name 081','member081@gmail.com','070-3548-0081');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id082','123456','Member Name 082','member082@gmail.com','070-3548-0082');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id083','123456','Member Name 083','member083@gmail.com','070-3548-0083');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id084','123456','Member Name 084','member084@gmail.com','070-3548-0084');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id085','123456','Member Name 085','member085@gmail.com','070-3548-0085');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id086','123456','Member Name 086','member086@gmail.com','070-3548-0086');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id087','123456','Member Name 087','member087@gmail.com','070-3548-0087');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id088','123456','Member Name 088','member088@gmail.com','070-3548-0088');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id089','123456','Member Name 089','member089@gmail.com','070-3548-0089');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id090','123456','Member Name 090','member090@gmail.com','070-3548-0090');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id091','123456','Member Name 091','member091@gmail.com','070-3548-0091');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id092','123456','Member Name 092','member092@gmail.com','070-3548-0092');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id093','123456','Member Name 093','member093@gmail.com','070-3548-0093');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id094','123456','Member Name 094','member094@gmail.com','070-3548-0094');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id095','123456','Member Name 095','member095@gmail.com','070-3548-0095');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id096','123456','Member Name 096','member096@gmail.com','070-3548-0096');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id097','123456','Member Name 097','member097@gmail.com','070-3548-0097');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id098','123456','Member Name 098','member098@gmail.com','070-3548-0098');
+INSERT INTO MEMBER(login_id, password,member_name, mail_address, phone_number) VALUES
+('id099','123456','Member Name 099','member099@gmail.com','070-3548-0099');
