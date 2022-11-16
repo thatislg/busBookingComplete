666c5e23ec7db0166446a9a655b9e82387212c5d
thatislg <lnllnl01111@gmail.com>
2022-11-16 20:11:35 +0900

    Cập nhật SQL

---
+++ root/common/sql/1_bus_data.sql
@@ -0,0 +1,325 @@
+-- BUS TABLE'S SAMPLES DATA
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'群馬','280','あ','1','99');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'大宮','281','い','2','98');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'所沢','282','う','3','97');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'熊谷','283','え','4','96');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'春日部','284','お','5','95');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'千葉','285','さ','6','94');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'習志野','286','し','7','93');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'袖ヶ浦','287','す','8','92');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'野田','288','せ','9','91');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'品川','289','そ','10','90');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'練馬','290','か','11','89');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'足立','291','き','12','88');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'八王子','292','く','13','87');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'多摩','293','け','14','86');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'横浜','294','こ','15','85');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'川崎','295','な','16','84');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'湘南','296','に','17','83');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'相模','297','ぬ','18','82');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'和歌山','100','の','19','81');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'鳥取','101','ま','20','80');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'島根','102','み','21','79');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'岡山','103','む','22','78');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'広島','104','め','23','77');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'福山','105','も','24','76');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'山口','106','ざ','25','75');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'徳島','107','ず','26','74');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'香川','108','ぞ','27','73');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'愛媛','109','は','28','72');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'高知','110','ひ','29','71');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'福岡','111','ふ','30','70');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'北九州','112','へ','31','69');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'久留米','113','ほ','32','68');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'筑豊','114','た','33','67');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'佐賀','115','ち','34','66');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'長崎','116','つ','35','65');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'佐世保','117','て','36','64');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'群馬','118','あ','37','63');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'大宮','119','い','38','62');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'所沢','120','う','39','61');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'熊谷','121','え','40','60');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'春日部','122','お','41','59');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'千葉','123','さ','42','58');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'習志野','124','し','43','57');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'袖ヶ浦','125','す','44','56');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'野田','126','せ','45','55');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'品川','127','そ','46','54');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'練馬','128','か','47','53');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'足立','129','き','48','52');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'八王子','130','く','49','51');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'多摩','131','け','50','50');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'横浜','132','こ','51','49');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'川崎','133','な','52','48');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'湘南','134','に','53','47');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'相模','135','ぬ','54','46');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'和歌山','136','の','55','45');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'鳥取','137','ま','56','44');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'島根','138','み','57','43');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'岡山','139','む','58','42');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'広島','140','め','59','41');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'福山','141','も','60','40');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'山口','142','ざ','61','39');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'徳島','143','ず','62','38');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'香川','144','ぞ','63','37');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'愛媛','145','は','64','36');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'高知','146','ひ','65','35');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'福岡','147','ふ','66','34');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'北九州','148','へ','67','33');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'久留米','149','ほ','68','32');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'筑豊','150','た','69','31');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'佐賀','151','ち','70','30');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'長崎','152','つ','71','29');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'佐世保','153','て','72','28');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'群馬','154','あ','73','27');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'大宮','155','い','74','26');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'所沢','156','う','75','25');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'熊谷','157','え','76','24');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'春日部','158','お','77','23');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'千葉','159','さ','78','22');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'習志野','160','し','79','21');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'袖ヶ浦','161','す','80','20');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'野田','162','せ','81','19');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'品川','163','そ','82','18');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'練馬','164','か','83','17');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'足立','165','き','84','16');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'八王子','166','く','85','15');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'多摩','167','け','86','14');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'横浜','168','こ','87','13');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'川崎','169','な','88','12');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'湘南','170','に','89','11');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'相模','171','ぬ','90','10');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'和歌山','172','の','91','9');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'鳥取','173','ま','92','8');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'島根','174','み','93','7');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'岡山','175','む','94','6');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'広島','176','め','95','5');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'福山','177','も','96','4');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'山口','178','ざ','97','3');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'徳島','179','ず','98','2');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'香川','180','ぞ','99','1');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'愛媛','181','は','1','99');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'高知','182','ひ','2','98');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'福岡','183','ふ','3','97');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'北九州','184','へ','4','96');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'久留米','185','ほ','5','95');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'筑豊','186','た','6','94');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(11,4,'佐賀','187','ち','7','93');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(6,4,'長崎','188','つ','8','92');
+INSERT INTO bus
+(row_num, column_num, number_place, number_division, number_kana, number_first_half, number_latter_half)
+VALUES(5,4,'佐世保','189','て','9','91');
