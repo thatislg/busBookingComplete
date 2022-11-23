a90f9d22c22cb231b235ed081bf37fdf58f93257
kiku88 <112387295+kiku88@users.noreply.github.com>
2022-11-23 13:03:34 +0900

    Commit thieu file chức năng 日別路線予約状況

--- root/backend/src/main/java/vn/com/vti/bus/backend/controller/ReservationController.java
+++ root/backend/src/main/java/vn/com/vti/bus/backend/controller/ReservationController.java
@@ -11,48 +11,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import vn.com.vti.bus.entity.Bus;
-import vn.com.vti.bus.entity.BusStation;
-import vn.com.vti.bus.entity.Member;
-import vn.com.vti.bus.entity.Reserve;
-import vn.com.vti.bus.entity.ReserveExample;
-import vn.com.vti.bus.entity.Route;
-import vn.com.vti.bus.entity.Seat;
-import vn.com.vti.bus.entity.SeatExample;
+import vn.com.vti.bus.entity.RouteCustom;
 import vn.com.vti.bus.entity.SeatOrder;
-import vn.com.vti.bus.mapper.BusMapper;
-import vn.com.vti.bus.mapper.BusStationMapper;
-import vn.com.vti.bus.mapper.MemberMapper;
-import vn.com.vti.bus.mapper.ReserveMapper;
-import vn.com.vti.bus.mapper.RouteMapper;
-import vn.com.vti.bus.mapper.SeatMapper;
+import vn.com.vti.bus.mapper.ReserveCustomMapper;
+import vn.com.vti.bus.mapper.RouteCustomMapper;
 
 @Controller
 @RequestMapping("/reservation")
 public class ReservationController {
 
 	@Autowired
-	private RouteMapper routeMapper;
+	private ReserveCustomMapper reserveCustomMapper;
+	
+	
 	@Autowired
-	private ReserveMapper reserveMapper;
-
-	@Autowired
-	private BusStationMapper busStationMapper;
-
-	@Autowired
-	private SeatMapper seatMapper;
-
-	@Autowired
-	private BusMapper busMapper;
-
-	@Autowired
-	private MemberMapper memberMapper;
+	private RouteCustomMapper routeCustomMapper;
+	
+	
 
 	@RequestMapping("/index")
 	public String index(@RequestParam String routeId, Model model) {
-		searchRouteId = routeId;
+		searchRouteId = Integer.parseInt(routeId);
 
-		searchSeatList(new Date(), model);
+		searchSeatList(new Date(), searchRouteId, model);
 
 		return "route/reservation";
 	}
@@ -68,69 +49,41 @@ public class ReservationController {
 			date = dateFormat.parse(dateStr);
 		}
 
-		searchSeatList(date, model);
+		searchSeatList(date, searchRouteId, model);
 
 		return "route/reservation";
 	}
 
-	private String searchRouteId;
+	private Integer searchRouteId;
 
-	private void searchSeatList(Date date, Model model) {
+	private void searchSeatList(Date date, Integer searchRouteId,  Model model) {
 
-		Route route = routeMapper.selectByPrimaryKey(Integer.parseInt(searchRouteId));
-		model.addAttribute("routeInfo", route);
-
-		BusStation departureStation = busStationMapper.selectByPrimaryKey(route.getDepartureId());
-		BusStation arrivalStation = busStationMapper.selectByPrimaryKey(route.getArrivalId());
-
-		model.addAttribute("departureStation", departureStation);
-		model.addAttribute("arrivalStation", arrivalStation);
-		model.addAttribute("routeId", searchRouteId);
+		RouteCustom routeInfo = routeCustomMapper.selectRouteInfoByRouteId(searchRouteId);
+		model.addAttribute("routeInfo", routeInfo);
 		model.addAttribute("date", date);
-		//予約リストを取得する
-		ReserveExample reserveExample = new ReserveExample();
-		reserveExample.createCriteria().andDepartureDateEqualTo(date)
-				.andRouteIdEqualTo(Integer.parseInt(searchRouteId));
-		List<Reserve> reserveList = reserveMapper.selectByExample(reserveExample);
-		model.addAttribute("reserveList", reserveList);
-
 		
+		
+		List<SeatOrder> seatOrderListTmp =  reserveCustomMapper.selectReservationByDepartureDate(date,searchRouteId);
+
 		//バスの座席を仮にセットする
-		Bus bus = busMapper.selectByPrimaryKey(route.getBusId());
-		model.addAttribute("bus", bus);
-		
 		List<SeatOrder> seatOrderList = new ArrayList<>();
-		Integer countSeat = bus.getRowNum() * bus.getColumnNum();
-		model.addAttribute("countSeat", countSeat);
-		for (int i = 0; i < countSeat ; i++) {
-
+		for (int i = 0; i < routeInfo.getRowNum() * routeInfo.getColumnNum() ; i++) {
 			seatOrderList.add(null);
-
 		}
-
-		//予約された座席の番号と予約メンバーの名前を取得する
-		for (Reserve reserve : reserveList) {
-
-			Member member = memberMapper.selectByPrimaryKey(reserve.getMemberId());
-
-			SeatExample seatExample = new SeatExample();
-			seatExample.createCriteria().andReserveIdEqualTo(reserve.getReserveId());
-			List<Seat> seatListByReserveId = seatMapper.selectByExample(seatExample);
-			for (Seat seat : seatListByReserveId) {
-				SeatOrder seatOrder = new SeatOrder();
-				seatOrder.setSeatNumber(seat.getSeatNumber());
-				seatOrder.setMemberName(member.getMemberName());
-				seatOrderList.set(seatOrder.getSeatNumber() - 1, seatOrder);
-			}
-
+		
+		//予約された座席の番号を仮にセットした座席に代入する
+		for(SeatOrder  seatOrder : seatOrderListTmp) {
+			
+			seatOrderList.set(seatOrder.getSeatNumber() - 1, seatOrder);
+			
 		}
-
+		
 		//1列ごとにリストの要素としてリストする
 		List<List<SeatOrder>> rowSeatList = new ArrayList<List<SeatOrder>>();
 		for (int i = 1; i < seatOrderList.size(); i++) {
 
-			if ((i % bus.getColumnNum() == 0) || (i == seatOrderList.size() - 1)) {
-				List<SeatOrder> rowSeat = seatOrderList.subList(i - bus.getColumnNum(), i);
+			if ((i % routeInfo.getColumnNum() == 0) || (i == seatOrderList.size() - 1)) {
+				List<SeatOrder> rowSeat = seatOrderList.subList(i - routeInfo.getColumnNum(), i);
 				rowSeatList.add(rowSeat);
 			}
 
